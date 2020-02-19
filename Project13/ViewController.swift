5a00d944ef708769a00472d035b792c572d5fbcf
VBondarev <freddiebo@yandex.ru>
2020-02-19 07:45:43 +0300

    Initial Commit

---
+++ Project13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by  Vladislav Bondarev on 19.02.2020.
+//  Copyright © 2020 Vladislav Bondarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
